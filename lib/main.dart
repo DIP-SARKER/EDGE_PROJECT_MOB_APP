cb4635de2d2dbdc748656cc6c2e85ca518940e5a
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-03-17 23:41:08 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color.fromARGB(255, 119, 82, 223),
+        disabledColor: Colors.grey[600],
+        hintColor: const Color.fromARGB(255, 42, 15, 117),
+      ),
+      // home: SplashScreen(),
+    );
+  }
+}
